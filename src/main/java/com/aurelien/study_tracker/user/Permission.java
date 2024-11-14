4361bf0aee33429c0803cee950b16b5169192fbb
Aurelien Ebeng <100911954+AurelienEbeng@users.noreply.github.com>
2024-11-14 18:53:10 -0500

    Created enum Permissions. It holds all the permissions of the app

---
+++ src/main/java/com/aurelien/study_tracker/user/Permission.java
@@ -0,0 +1,24 @@
+package com.aurelien.study_tracker.user;
+
+public enum Permission {
+
+    SESSION_CREATE,
+
+    TASK_CREATE,
+
+    TASK_GET_ALL_FOR_ID,
+
+    HIGHLIGHT_GET,
+
+    USER_DETAILS_GET_FOR_ID,
+
+    DEMO_USER_LOGIN,
+
+    DEMO_USER_CREATE,
+
+    FORGOT_PASSWORD,
+
+    RESET_PASSWORD,
+
+    VERIFY_EMAIL
+}
