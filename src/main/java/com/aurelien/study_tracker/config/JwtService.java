898d75b469e73080021a3c64a21fdf3746b771c8
Aurelien Ebeng <100911954+AurelienEbeng@users.noreply.github.com>
2024-11-14 21:27:09 -0500

    Added JwtService

---
+++ src/main/java/com/aurelien/study_tracker/config/JwtService.java
@@ -0,0 +1,55 @@
+package com.aurelien.study_tracker.config;
+
+
+import com.aurelien.study_tracker.user.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.Map;
+
+@Service
+public class JwtService {
+
+    @Value("${security.jwt.expiration-minutes}")
+    private long EXPIRATION_MINUTES;
+
+    @Value("${security.jwt.secret-key}")
+    private String SECRET_KEY;
+
+
+    public String generateToken(User user, Map<String, Object> extraClaims) {
+        Date issuedAt = new Date(System.currentTimeMillis());
+        Date expiration = new Date(issuedAt.getTime() + (EXPIRATION_MINUTES * 60 * 1000));
+        return Jwts.builder()
+                .setClaims(extraClaims)
+                .setSubject(user.getEmail())
+                .setIssuedAt(issuedAt)
+                .setExpiration(expiration)
+                .signWith(generateKey(), SignatureAlgorithm.HS512)
+                .compact();
+    }
+
+
+    private Key generateKey() {
+        byte[] secretAsBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(secretAsBytes);
+    }
+
+    public String extractEmail(String jwt) {
+        return extractAllClaims(jwt).getSubject();
+    }
+
+    private Claims extractAllClaims(String jwt) {
+        return Jwts.parser().setSigningKey(generateKey()).build()
+                .parseClaimsJws(jwt).getBody();
+    }
+
+
+}
