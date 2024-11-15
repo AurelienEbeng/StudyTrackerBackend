a588bf321f6160e2322e730d63151192c73cde3e
Aurelien Ebeng <100911954+AurelienEbeng@users.noreply.github.com>
2024-11-14 21:53:11 -0500

    Created AuthenticationController

---
+++ src/main/java/com/aurelien/study_tracker/authentication/AuthenticationController.java
@@ -0,0 +1,31 @@
+package com.aurelien.study_tracker.authentication;
+
+import com.aurelien.study_tracker.user.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthenticationController {
+
+    private final AuthenticationService authenticationService;
+
+
+    public AuthenticationController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody User request){
+        return ResponseEntity.ok(authenticationService.register(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request){
+        return ResponseEntity.ok(authenticationService.login(request));
+    }
+}
