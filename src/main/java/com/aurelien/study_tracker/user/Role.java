12c87c196f2bdd72c55c44cbfa0bd9204ba92b2e
Aurelien Ebeng <100911954+AurelienEbeng@users.noreply.github.com>
2024-11-14 19:19:59 -0500

    Created the admin, user and demo_user roles. The roles have been given appropriate permissions

---
+++ src/main/java/com/aurelien/study_tracker/user/Role.java
@@ -0,0 +1,54 @@
+package com.aurelien.study_tracker.user;
+
+import java.util.Arrays;
+import java.util.List;
+
+public enum Role {
+    ADMIN(Arrays.asList(
+            Permission.SESSION_CREATE,
+            Permission.TASK_CREATE,
+            Permission.TASK_GET_ALL_FOR_ID,
+            Permission.HIGHLIGHT_GET,
+            Permission.USER_DETAILS_GET_FOR_ID,
+            Permission.DEMO_USER_LOGIN,
+            Permission.DEMO_USER_CREATE,
+            Permission.FORGOT_PASSWORD,
+            Permission.RESET_PASSWORD,
+            Permission.VERIFY_EMAIL)),
+
+    USER(Arrays.asList(
+            Permission.SESSION_CREATE,
+            Permission.TASK_CREATE,
+            Permission.TASK_GET_ALL_FOR_ID,
+            Permission.HIGHLIGHT_GET,
+            Permission.USER_DETAILS_GET_FOR_ID,
+            Permission.FORGOT_PASSWORD,
+            Permission.RESET_PASSWORD,
+            Permission.VERIFY_EMAIL)),
+
+    DEMO_USER(Arrays.asList(
+            Permission.SESSION_CREATE,
+            Permission.TASK_CREATE,
+            Permission.TASK_GET_ALL_FOR_ID,
+            Permission.HIGHLIGHT_GET,
+            Permission.USER_DETAILS_GET_FOR_ID,
+            Permission.DEMO_USER_LOGIN,
+            Permission.FORGOT_PASSWORD,
+            Permission.RESET_PASSWORD,
+            Permission.VERIFY_EMAIL));
+
+
+    private List<Permission> permissions;
+
+    Role(List<Permission> permissions) {
+        this.permissions = permissions;
+    }
+
+    public List<Permission> getPermissions() {
+        return permissions;
+    }
+
+    public void setPermissions(List<Permission> permissions) {
+        this.permissions = permissions;
+    }
+}
