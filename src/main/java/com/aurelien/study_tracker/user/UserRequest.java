e22f8f915cc945753cb27e009e894b24962bbbd8
Aurelien Ebeng <100911954+AurelienEbeng@users.noreply.github.com>
2024-11-15 06:50:15 -0500

    Created UserRequest

---
+++ src/main/java/com/aurelien/study_tracker/user/UserRequest.java
@@ -0,0 +1,14 @@
+package com.aurelien.study_tracker.user;
+
+public class UserRequest {
+
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
