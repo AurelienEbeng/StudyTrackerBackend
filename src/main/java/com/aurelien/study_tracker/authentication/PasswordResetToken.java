16d9a222da6e7036906999543d3f02b804e4c478
Aurelien Ebeng <100911954+AurelienEbeng@users.noreply.github.com>
2024-11-16 09:47:57 -0500

    A user is not more deleted when a password reset token is deleted

--- src/main/java/com/aurelien/study_tracker/authentication/PasswordResetToken.java
+++ src/main/java/com/aurelien/study_tracker/authentication/PasswordResetToken.java
@@ -16,7 +16,7 @@ public class PasswordResetToken {
     private int id;
     private String token;
     private LocalDateTime expiryDateTime;
-    @OneToOne(fetch = FetchType.EAGER , cascade = CascadeType.ALL)
+    @OneToOne
     @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false)
     private User user;
 
